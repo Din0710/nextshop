3954e1de32caa00f1c82e17c90e2e383932c9cdb
Din0710 <121253176+Din0710@users.noreply.github.com>
2022-12-29 16:43:17 +0900

    2nd-List Product

---
+++ components/ProductItem.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <img
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow"
+        />
+      </Link>
+      <div>
+        <Link href={`/product/${product.slug}`} className="text-lg">
+          <h2> {product.name}</h2>
+        </Link>
+        <p className="mb-2"> {product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-buttom" type="button">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,19 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
+import ProductItem from '../components/Productitem'
+import data from '../utils/data'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Layout title="Home">
-      <h1 className="text-3xl text-blue-500 bg-yellow-300">NextShop</h1>
-      <p>Author: Jaloliddin</p>
-      <p>archa bayram yaqin</p>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
     </Layout>
   )
 }
